b554aed69301a7b16faed760a392fe25c7a015f6
Near Huscarl <near.huscarl@gmail.com>
2017-10-27 02:07:11 +0700

    get search api working

--- wikipedia-viewer/src/js/main.js
+++ wikipedia-viewer/src/js/main.js
@@ -1,12 +1,25 @@
 var wikipediaViewer = (function(){
+   // option for number of search result (dropdown)
 
    $(document).ready(function(){
       $('.btn__random').on('click', function() {
          var url = 'https://en.wikipedia.org/wiki/Special:Random';
          window.open(url, '_blank');
       });
-
       $('.icon__search').on('click', function() {
+         var crossover = 'https://cors-anywhere.herokuapp.com';
+         var query = $('#inlineFormInputGroup').val();
+         var apiSearch =  crossover + '/en.wikipedia.org/w/api.php?action=opensearch&search='
+            + query + '&format=json';
+
+         $.getJSON(apiSearch, function(searchData){
+            var title   = searchData[1][0];
+            var summary = searchData[2][0];
+
+            alert(title);
+            alert(summary);
+            alert(link);
+         });
 
       });
    });
