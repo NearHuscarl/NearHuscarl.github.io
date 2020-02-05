b550a6098b7c24386056eb6bc8ba8f12aca832fe
NearHuscarl <near.huscarl@gmail.com>
2020-02-05 16:24:11 +0700

    chore: rewrite audit script
    - Audit all pages
    - Generate html reports in audit/
    - Audit deployed url on netlify instead of on localhost

--- scripts/audit.js
+++ scripts/audit.js
@@ -1,12 +1,37 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const shell = require('shelljs');
 const fs = require('fs');
 const { promisify } = require('util');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
-const server = require('./server.js');
-const config = require('./desktop-config.js');
+const desktopConfig = require('./desktop-config.js');
 
 const writeFileAsync = promisify(fs.writeFile);
+const readdirAsync = promisify(fs.readdir);
+
+function removeFilenameExtension(path) {
+	return path.replace(/\.[^/.]+$/, '');
+}
+function trimTrailing0(uglyFloat) {
+	return parseFloat(uglyFloat.toFixed(2));
+}
+
+async function getRoutes() {
+	const files = await readdirAsync('src/pages');
+	return files.map((file) => {
+		if (file.startsWith('index')) return '/';
+		return '/' + removeFilenameExtension(file);
+	});
+}
+
+async function writeFile(path, content) {
+	await writeFileAsync(path, content, (err) => {
+		if (err) {
+			return console.error(err);
+		}
+		return '';
+	});
+}
 
 function launchChromeAndRunLighthouse(url, opts, config = null) {
 	return chromeLauncher
@@ -18,7 +43,7 @@ function launchChromeAndRunLighthouse(url, opts, config = null) {
 				// https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
 				// use results.report for the HTML/JSON/CSV output as a string
 				// use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-				return chrome.kill().then(() => results.lhr);
+				return chrome.kill().then(() => results);
 			});
 		});
 }
@@ -45,48 +70,73 @@ function getColor(score) {
 	}
 	return 'red';
 }
-async function createBadgeConfig({ id, title, score }) {
-	const configPath = `audit/${id}.json`;
-	const config = {
-		schemaVersion: 1,
-		label: title,
-		message: score.toString(),
-		color: getColor(score),
-	};
-
-	await writeFileAsync(configPath, JSON.stringify(config), (err) => {
-		if (err) {
-			return console.log(err);
-		}
-		return '';
-	});
-}
 
 const opts = {
+	output: 'html',
 	chromeFlags: [],
 };
-async function execAuditing() {
-	await new Promise((resolve) => server.listen('9000', resolve));
-	console.log('App is listening on port 9000');
-
-	await launchChromeAndRunLighthouse(
-		'http://localhost:9000',
+async function execAuditing(route) {
+	return launchChromeAndRunLighthouse(
+		`http://near.netlify.com${route}`,
 		opts,
-		config,
-	).then(async (results) => {
-		const categories = Object.keys(results.categories);
+		desktopConfig,
+	).then((results) => {
+		const { lhr, report } = results;
+		const scoreReport = {};
 
-		for (const category of categories) {
-			await createBadgeConfig(results.categories[category]);
+		Object.keys(lhr.categories).forEach((category) => {
+			const { id, title, score } = lhr.categories[category];
+			scoreReport[id] = { title, score };
+		});
+
+		if (route === '/') {
+			writeFile('audit/report.html', report);
+		} else {
+			writeFile(`audit/report-${route.replace(/^\//, '')}.html`, report);
 		}
-
-		await new Promise((resolve) => server.close(resolve));
+		return scoreReport;
 	});
 }
 
 async function main() {
-	shell.exec('yarn build');
-	await execAuditing();
+	// shell.exec('yarn build');
+
+	const routes = await getRoutes();
+	const reports = {};
+
+	// eslint-disable-next-line no-restricted-syntax
+	for (const route of routes) {
+		// if we don't await, all of the chrome browsers will launch for each route at the same time
+		// eslint-disable-next-line no-await-in-loop
+		reports[route] = await execAuditing(route);
+	}
+
+	const scores = {};
+	const categoryIds = Object.keys(reports['/']);
+
+	categoryIds.forEach((categoryId) => {
+		scores[categoryId] = 0;
+	});
+	routes.forEach((route) => {
+		const report = reports[route];
+
+		categoryIds.forEach((categoryId) => {
+			scores[categoryId] += report[categoryId].score;
+		});
+	});
+
+	categoryIds.forEach((categoryId) => {
+		const configPath = `audit/${categoryId}.json`;
+		const score = scores[categoryId];
+		const badgeConfig = {
+			schemaVersion: 1,
+			label: reports['/'][categoryId].title,
+			message: trimTrailing0(score / routes.length).toString(),
+			color: getColor(score),
+		};
+
+		writeFile(configPath, JSON.stringify(badgeConfig));
+	});
 }
 
 main();
