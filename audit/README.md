f84f385fcdc1509459f7e8b4f0cbd40add84b6a5
NearHuscarl <near.huscarl@gmail.com>
2020-02-06 11:46:58 +0700

    chore: add README.md report for auditing

---
+++ audit/README.md
@@ -0,0 +1,74 @@
+# Lighthouse Reports
+
+Reported at commit [`bc09338`](https://github.com/NearHuscarl/portfolio/commit/bc093383de3870657849effeae8f680315371dca)
+
+## [`near.netlify.com`](https://near.netlify.com)
+
+| Category | Score |
+|----------|------:|
+| Accessibility | 95 |
+| Best Practices | 92 |
+| Performance | 100 |
+| Progressive Web App | 65 |
+| SEO | 100 |
+
+[Full Report](https://htmlpreview.github.io/?https://github.com/NearHuscarl/portfolio/blob/master/audit/report.html)
+
+## [`near.netlify.com/about`](https://near.netlify.com/about)
+
+| Category | Score |
+|----------|------:|
+| Accessibility | 94 |
+| Best Practices | 92 |
+| Performance | 100 |
+| Progressive Web App | 65 |
+| SEO | 100 |
+
+[Full Report](https://htmlpreview.github.io/?https://github.com/NearHuscarl/portfolio/blob/master/audit/report-about.html)
+
+## [`near.netlify.com/resume`](https://near.netlify.com/resume)
+
+| Category | Score |
+|----------|------:|
+| Accessibility | 94 |
+| Best Practices | 92 |
+| Performance | 100 |
+| Progressive Web App | 65 |
+| SEO | 100 |
+
+[Full Report](https://htmlpreview.github.io/?https://github.com/NearHuscarl/portfolio/blob/master/audit/report-resume.html)
+
+## [`near.netlify.com/resume-full`](https://near.netlify.com/resume-full)
+
+| Category | Score |
+|----------|------:|
+| Accessibility | 94 |
+| Best Practices | 92 |
+| Performance | 100 |
+| Progressive Web App | 65 |
+| SEO | 100 |
+
+[Full Report](https://htmlpreview.github.io/?https://github.com/NearHuscarl/portfolio/blob/master/audit/report-resume-full.html)
+
+## [`near.netlify.com/404`](https://near.netlify.com/404)
+
+| Category | Score |
+|----------|------:|
+| Accessibility | 94 |
+| Best Practices | 92 |
+| Performance | 100 |
+| Progressive Web App | 65 |
+| SEO | 100 |
+
+[Full Report](https://htmlpreview.github.io/?https://github.com/NearHuscarl/portfolio/blob/master/audit/report-404.html)
+
+## Average Score
+
+| Category | Score |
+|----------|------:|
+| Accessibility | 94.2 |
+| Best Practices | 92 |
+| Performance | 100 |
+| Progressive Web App | 65 |
+| SEO | 100 |
+
--- scripts/audit.js
+++ scripts/audit.js
@@ -1,6 +1,9 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable import/no-extraneous-dependencies */
 const shell = require('shelljs');
 const fs = require('fs');
+const fetch = require('node-fetch');
 const { promisify } = require('util');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
@@ -8,6 +11,7 @@ const desktopConfig = require('./desktop-config.js');
 
 const writeFileAsync = promisify(fs.writeFile);
 const readdirAsync = promisify(fs.readdir);
+const appendFileAsync = promisify(fs.appendFile);
 
 function removeFilenameExtension(path) {
 	return path.replace(/\.[^/.]+$/, '');
@@ -71,11 +75,16 @@ function getColor(score) {
 	return 'red';
 }
 
-const opts = {
-	output: 'html',
-	chromeFlags: [],
-};
+function getHtmlReportFile(route) {
+	if (route === '/') {
+		return 'report.html';
+	}
+	return `report-${route.replace(/^\//, '')}.html`;
+}
+
 async function execAuditing(route) {
+	const opts = { output: 'html', chromeFlags: [] };
+
 	return launchChromeAndRunLighthouse(
 		`http://near.netlify.com${route}`,
 		opts,
@@ -89,13 +98,101 @@ async function execAuditing(route) {
 			scoreReport[id] = { title, score };
 		});
 
-		if (route === '/') {
-			writeFile('audit/report.html', report);
-		} else {
-			writeFile(`audit/report-${route.replace(/^\//, '')}.html`, report);
-		}
+		writeFile(`audit/${getHtmlReportFile(route)}`, report);
 		return scoreReport;
 	});
+}
+
+// calculate average scores for each category from all routes
+// and update the config files to display the badges
+function getAverageScores(reports) {
+	const routes = Object.keys(reports);
+	const scores = {};
+	const categoryIds = Object.keys(reports['/']);
+
+	categoryIds.forEach((categoryId) => {
+		scores[categoryId] = {
+			score: 0,
+			title: reports['/'][categoryId].title,
+		};
+	});
+	routes.forEach((route) => {
+		const report = reports[route];
+
+		categoryIds.forEach((categoryId) => {
+			scores[categoryId].score += report[categoryId].score;
+		});
+	});
+
+	categoryIds.forEach((categoryId) => {
+		scores[categoryId].score /= routes.length;
+	});
+
+	return scores;
+}
+
+async function updateAuditReadme(reports, avgScores) {
+	const routes = Object.keys(reports).sort((a, b) => {
+		// index first, 404 last, the rest is in alphabetical ordered
+		if (a === '/') return -1;
+		if (b === '/') return 1;
+		if (a === '/404') return 1;
+		if (b === '/404') return -1;
+		return a > b;
+	});
+	const categories = Object.keys(reports[routes[0]]).sort();
+	const rootUrl = 'near.netlify.com';
+	const readmePath = 'audit/README.md';
+	const append = (line) => appendFileAsync(readmePath, line + '\n');
+
+	// clear old content
+	writeFileAsync(readmePath, '');
+
+	await append('# Lighthouse Reports');
+	await append('');
+
+	const siteId = '6463a76d-ab2e-41df-8383-fd816f82e3ba';
+	const deployInfo = await fetch(
+		`https://api.netlify.com/api/v1/sites/${siteId}`,
+	)
+		.then((res) => res.json())
+		.then((json) => json.published_deploy);
+	const commitRef = deployInfo.commit_ref.substring(0, 7);
+	const commitUrl = deployInfo.commit_url;
+	await append(`Reported at commit [\`${commitRef}\`](${commitUrl})`);
+	await append('');
+
+	for (const route of routes) {
+		const url = rootUrl + route.replace(/^\/$/, '');
+
+		await append(`## [\`${url}\`](https://${url})`);
+		await append('');
+		await append('| Category | Score |');
+		await append('|----------|------:|');
+
+		for (const category of categories) {
+			const { title, score } = reports[route][category];
+			await append(`| ${title} | ${score * 100} |`);
+		}
+		await append('');
+		await append(
+			`[Full Report](https://htmlpreview.github.io/?https://github.com/NearHuscarl/portfolio/blob/master/audit/${getHtmlReportFile(
+				route,
+			)})`,
+		);
+		await append('');
+	}
+
+	await append('## Average Score');
+	await append('');
+	await append('| Category | Score |');
+	await append('|----------|------:|');
+
+	for (const category of categories) {
+		const { title, score } = avgScores[category];
+		await append(`| ${title} | ${trimTrailing0(score * 100).toString()} |`);
+	}
+	await append('');
 }
 
 async function main() {
@@ -104,40 +201,66 @@ async function main() {
 	const routes = await getRoutes();
 	const reports = {};
 
-	// eslint-disable-next-line no-restricted-syntax
 	for (const route of routes) {
 		// if we don't await, all of the chrome browsers will launch for each route at the same time
-		// eslint-disable-next-line no-await-in-loop
 		reports[route] = await execAuditing(route);
 	}
 
-	// calculate average scores for each category from all routes
-	const scores = {};
+	// Uncomment to quickly debug
+	// const reports = {
+	// 	'/404': {
+	// 		performance: { title: 'Performance', score: 1 },
+	// 		accessibility: { title: 'Accessibility', score: 0.94 },
+	// 		'best-practices': { title: 'Best Practices', score: 0.92 },
+	// 		seo: { title: 'SEO', score: 1 },
+	// 		pwa: { title: 'Progressive Web App', score: 0.65 },
+	// 	},
+	// 	'/about': {
+	// 		performance: { title: 'Performance', score: 1 },
+	// 		accessibility: { title: 'Accessibility', score: 0.94 },
+	// 		'best-practices': { title: 'Best Practices', score: 0.92 },
+	// 		seo: { title: 'SEO', score: 1 },
+	// 		pwa: { title: 'Progressive Web App', score: 0.65 },
+	// 	},
+	// 	'/': {
+	// 		performance: { title: 'Performance', score: 1 },
+	// 		accessibility: { title: 'Accessibility', score: 0.95 },
+	// 		'best-practices': { title: 'Best Practices', score: 0.92 },
+	// 		seo: { title: 'SEO', score: 1 },
+	// 		pwa: { title: 'Progressive Web App', score: 0.65 },
+	// 	},
+	// 	'/resume-full': {
+	// 		performance: { title: 'Performance', score: 1 },
+	// 		accessibility: { title: 'Accessibility', score: 0.94 },
+	// 		'best-practices': { title: 'Best Practices', score: 0.92 },
+	// 		seo: { title: 'SEO', score: 1 },
+	// 		pwa: { title: 'Progressive Web App', score: 0.65 },
+	// 	},
+	// 	'/resume': {
+	// 		performance: { title: 'Performance', score: 1 },
+	// 		accessibility: { title: 'Accessibility', score: 0.94 },
+	// 		'best-practices': { title: 'Best Practices', score: 0.92 },
+	// 		seo: { title: 'SEO', score: 1 },
+	// 		pwa: { title: 'Progressive Web App', score: 0.65 },
+	// 	},
+	// };
 	const categoryIds = Object.keys(reports['/']);
-
-	categoryIds.forEach((categoryId) => {
-		scores[categoryId] = 0;
-	});
-	routes.forEach((route) => {
-		const report = reports[route];
-
-		categoryIds.forEach((categoryId) => {
-			scores[categoryId] += report[categoryId].score;
-		});
-	});
+	const avgScores = getAverageScores(reports);
 
 	categoryIds.forEach((categoryId) => {
 		const configPath = `audit/${categoryId}.json`;
-		const score = scores[categoryId];
+		const { title, score } = avgScores[categoryId];
 		const badgeConfig = {
 			schemaVersion: 1,
-			label: reports['/'][categoryId].title,
-			message: trimTrailing0((score * 100) / routes.length).toString(),
+			label: title,
+			message: trimTrailing0(score * 100).toString(),
 			color: getColor(score),
 		};
 
 		writeFile(configPath, JSON.stringify(badgeConfig));
 	});
+
+	updateAuditReadme(reports, avgScores);
 }
 
 main();
