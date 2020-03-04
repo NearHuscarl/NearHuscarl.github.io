8c5a14a8b03ffc8a508536b2a570e8c4e774b0a1
NearHuscarl <near.huscarl@gmail.com>
2020-03-04 11:46:37 +0700

    chore: update portolio link

--- src/data/media.ts
+++ src/data/media.ts
@@ -1,5 +1,5 @@
 const media = {
-	website: 'https://nearhuscarl.github.io/portfolio',
+	website: 'https://near.netlify.com',
 	github: 'https://github.com/nearhuscarl',
 	stackoverflow: 'https://stackoverflow.com/users/9449426',
 	linkedIn: 'https://linkedin.com/in/near-huscarl',
